6c03dfd76214bc5c380ba47117e8840774bf6f3e
Kolesnikov <kolesnikovtimyrweb@gmail.com>
2024-11-24 12:43:03 +0100

    first commit

---
+++ app/dasboardWrapper.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./(components)/Navbar/page";
+import SideBar from "./(components)/Sidebar/page";
+
+const DasboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* SideBar */}
+      <SideBar />
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
+      >
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DasboardWrapper;
